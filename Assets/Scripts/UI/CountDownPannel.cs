1cb12e68cd7a6cd1569cad3bd39bf826fd074ace
city0409 <250924125@qq.com>
2018-01-15 17:43:07 +0800

    0115倒计时321、闪烁touch

---
+++ Assets/Scripts/UI/CountDownPannel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class CountDownPannel : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject[] numberText;
+    private Tweener ter;
+
+    private void Awake()
+    {
+        
+    } 
+    private void Start()
+    {
+
+            StartCoroutine(Stop());
+
+
+    }
+    private IEnumerator Stop()
+    {
+        for (int i = 0; i < numberText.Length; i++)
+        {
+            numberText[i].SetActive(true);
+            //numberText[i].transform.GetComponent<Text>().material.color = new Color(1, 1, 1, 1);
+            Sequence seq = DOTween.Sequence();
+            seq.Append(numberText[i].transform.DOScale(new Vector3(2, 2, 2), 0.5f));
+            seq.Append(numberText[i].transform.GetComponent<Text>().DOColor(new Color(1, 1, 1, 0), 0.5f));
+            yield return new WaitForSeconds(1.5f);
+
+        }
+    }
+}
---
+++ Assets/Scripts/UI/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    private Tweener ter;
+
+    private void Start()
+    {
+        //transform.GetComponent<Text>().material.color = new Color(1, 1, 1, 0);
+        Sequence seq = DOTween.Sequence();
+        seq.Append(transform.GetComponent<Text>().DOColor(new Color(1, 1, 1, 1), 0.6f));
+        seq.Append(transform.GetComponent<Text>().DOColor(new Color(1, 1, 1, 0), 0.6f));
+        seq.SetLoops(-1);
+        //seq.Play();
+    }
+
+    private void Update()
+    {
+
+    }
+}
